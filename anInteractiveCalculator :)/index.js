83dd93e24fac0e53fe0af8b0e07c068b7a8d3a7f
Leo-Youmbi <leoyoumbi83@gmail.com>
2023-12-04 21:38:44 +0100

    props basics

--- anInteractiveCalculator :)/index.js
+++ anInteractiveCalculator :)/index.js
@@ -1,10 +1,10 @@
-const  Hello = () => {
+const  Hello = (props) => {
     return (
     <div>
-    <h1>Hello React, Hi</h1>    
+    <h1>Hello React, Hi {props.name}. I'm {props.age} years old</h1>    
     </div>
     )
 }
 // const hello = React.createElement('h1', {}, 'Hello React');
-ReactDOM.render(<Hello/>, document.querySelector(".root"));
+ReactDOM.render(<Hello name='bob' age={11}/>, document.querySelector(".root"));
 // ReactDOM.render(hello(), document.querySelector(".root"));
--- anInteractiveCalculator :)/lib/index.js
+++ anInteractiveCalculator :)/lib/index.js
@@ -1,8 +1,11 @@
 "use strict";
 
-var Hello = function Hello() {
-  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Hello React, Hi"));
+var Hello = function Hello(props) {
+  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Hello React, Hi ", props.name, ". I'm ", props.age, " years old"));
 };
 // const hello = React.createElement('h1', {}, 'Hello React');
-ReactDOM.render( /*#__PURE__*/React.createElement(Hello, null), document.querySelector(".root"));
+ReactDOM.render( /*#__PURE__*/React.createElement(Hello, {
+  name: "bob",
+  age: 11
+}), document.querySelector(".root"));
 // ReactDOM.render(hello(), document.querySelector(".root"));
