d5755bc3d2409e657e0dcb69355682362d212b0b
Leo-Youmbi <leoyoumbi83@gmail.com>
2023-12-04 22:20:28 +0100

    feat: building Calculator Visuals

--- anInteractiveCalculator :)/index.js
+++ anInteractiveCalculator :)/index.js
@@ -1,10 +1,32 @@
-const  Hello = (props) => {
+function Calcutor () {
     return (
-    <div>
-    <h1>Hello React, Hi {props.name}. I'm {props.age} years old</h1>    
-    </div>
+        <div className='calculator'>
+            <div className="display">0</div>
+            <CalcButton value={7}/>
+            <CalcButton value={8}/>
+            <CalcButton value={9}/>
+            <CalcButton className='operator' value="/"/>
+        
+            <CalcButton value={4}/>
+            <CalcButton value={5}/>
+            <CalcButton value={6}/>
+            <CalcButton className='operator' value="*"/>
+
+            <CalcButton value={1}/>
+            <CalcButton value={2}/>
+            <CalcButton value={3}/>
+            <CalcButton className='operator' value="-"/>
+
+            <CalcButton value='C'/>
+            <CalcButton value={0}/>
+            <CalcButton value='='/>
+            <CalcButton className='operator' value="+"/>
+        </div>
     )
 }
+function CalcButton (props) {
+    return <button className={props.className}>{props.value}</button>
+}
 // const hello = React.createElement('h1', {}, 'Hello React');
-ReactDOM.render(<Hello name='bob' age={11}/>, document.querySelector(".root"));
+ReactDOM.render(<div className='app-container'><Calcutor/></div>, document.querySelector(".root"));
 // ReactDOM.render(hello(), document.querySelector(".root"));
--- anInteractiveCalculator :)/lib/index.js
+++ anInteractiveCalculator :)/lib/index.js
@@ -1,11 +1,55 @@
 "use strict";
 
-var Hello = function Hello(props) {
-  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Hello React, Hi ", props.name, ". I'm ", props.age, " years old"));
-};
+function Calcutor() {
+  return /*#__PURE__*/React.createElement("div", {
+    className: "calculator"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "display"
+  }, "0"), /*#__PURE__*/React.createElement(CalcButton, {
+    value: 7
+  }), /*#__PURE__*/React.createElement(CalcButton, {
+    value: 8
+  }), /*#__PURE__*/React.createElement(CalcButton, {
+    value: 9
+  }), /*#__PURE__*/React.createElement(CalcButton, {
+    className: "operator",
+    value: "/"
+  }), /*#__PURE__*/React.createElement(CalcButton, {
+    value: 4
+  }), /*#__PURE__*/React.createElement(CalcButton, {
+    value: 5
+  }), /*#__PURE__*/React.createElement(CalcButton, {
+    value: 6
+  }), /*#__PURE__*/React.createElement(CalcButton, {
+    className: "operator",
+    value: "*"
+  }), /*#__PURE__*/React.createElement(CalcButton, {
+    value: 1
+  }), /*#__PURE__*/React.createElement(CalcButton, {
+    value: 2
+  }), /*#__PURE__*/React.createElement(CalcButton, {
+    value: 3
+  }), /*#__PURE__*/React.createElement(CalcButton, {
+    className: "operator",
+    value: "-"
+  }), /*#__PURE__*/React.createElement(CalcButton, {
+    value: "C"
+  }), /*#__PURE__*/React.createElement(CalcButton, {
+    value: 0
+  }), /*#__PURE__*/React.createElement(CalcButton, {
+    value: "="
+  }), /*#__PURE__*/React.createElement(CalcButton, {
+    className: "operator",
+    value: "+"
+  }));
+}
+function CalcButton(props) {
+  return /*#__PURE__*/React.createElement("button", {
+    className: props.className
+  }, props.value);
+}
 // const hello = React.createElement('h1', {}, 'Hello React');
-ReactDOM.render( /*#__PURE__*/React.createElement(Hello, {
-  name: "bob",
-  age: 11
-}), document.querySelector(".root"));
+ReactDOM.render( /*#__PURE__*/React.createElement("div", {
+  className: "app-container"
+}, /*#__PURE__*/React.createElement(Calcutor, null)), document.querySelector(".root"));
 // ReactDOM.render(hello(), document.querySelector(".root"));
