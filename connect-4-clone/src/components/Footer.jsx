791e93915718f55dbdcd10471738ce047ad74a6a
Leo-Youmbi <leoyoumbi83@gmail.com>
2023-12-09 14:06:53 +0100

    feat: implemented conditional rendering of footer buttons

--- connect-4-clone/src/components/Footer.jsx
+++ connect-4-clone/src/components/Footer.jsx
@@ -1,12 +1,15 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
+import {GAME_STATE_PLAYING} from "../Constants.js";
 
 // eslint-disable-next-line react/prop-types
-const Footer = ({newGameClickEvent, suggestClickEvent}) => {
+const Footer = ({newGameClickEvent, suggestClickEvent, gameState}) => {
+    let clickEvent;
+    let buttonText;
+    [clickEvent, buttonText] = gameState === GAME_STATE_PLAYING ? [suggestClickEvent, 'Suggest'] : [newGameClickEvent, 'New Game']
     return (
         <div className='panel footer'>
-            <button onClick={newGameClickEvent}>New Game</button>
-            <button onClick={suggestClickEvent}>Suggest</button>
+        <button type='button' onClick={clickEvent}>{buttonText}</button>
         </div>
     );
 };
--- connect-4-clone/src/components/GameBoard.jsx
+++ connect-4-clone/src/components/GameBoard.jsx
@@ -6,7 +6,7 @@ import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import isWinner, {getComputerMove, isDraw} from "../helper.js";
 import {
-    GAME_STATE_DRAW,
+    GAME_STATE_DRAW, GAME_STATE_IDLE,
     GAME_STATE_PLAYING,
     GAME_STATE_WIN,
     NO_PLAYER,
@@ -17,9 +17,9 @@ import {
 
 
 const GameBoard = () => {
-    const [gameBoard, setGameBoard] = useState(Array(16).fill(NO_PLAYER));
+    const [gameBoard, setGameBoard] = useState(Array(NUMBER_OF_CIRCLES).fill(NO_PLAYER));
     const [currentPlayer, setCurrentPlayer] = useState(PLAYER_1);
-    const [gameState, setGameState] = useState(GAME_STATE_PLAYING);
+    const [gameState, setGameState] = useState(GAME_STATE_IDLE);
     const [winPlayer, setWinPlayer] = useState(NO_PLAYER)
 
     useEffect(() => {
@@ -27,7 +27,7 @@ const GameBoard = () => {
     }, [])
 
     const initGame = () => {
-        setGameBoard(Array(16).fill(NO_PLAYER));
+        setGameBoard(Array(NUMBER_OF_CIRCLES).fill(NO_PLAYER));
         setCurrentPlayer(PLAYER_1);
         setGameState(GAME_STATE_PLAYING);
         setWinPlayer(NO_PLAYER);
@@ -51,15 +51,12 @@ const GameBoard = () => {
         else if (isDraw(gameBoard, id-1, currentPlayer)) {
             setGameState(GAME_STATE_DRAW);
             setWinPlayer(NO_PLAYER);
-        }
+        }  
 
         setGameBoard(prevState => {
             prevState[id-1] = currentPlayer
             return prevState
         });
-        let board = [...gameBoard]
-        board[id-1] = currentPlayer;
-        console.log(board);
        setCurrentPlayer(currentPlayer === PLAYER_1 ? PLAYER_2 : PLAYER_1);
     }
 
@@ -81,7 +78,7 @@ const GameBoard = () => {
             <div className="gameBoard">
                 {initBoard()}
             </div>
-            <Footer newGameClickEvent={newGameClicked} suggestClickEvent={suggestMove}/>
+            <Footer newGameClickEvent={newGameClicked} suggestClickEvent={suggestMove} gameState={gameState}/>
         </>
     )
 }
