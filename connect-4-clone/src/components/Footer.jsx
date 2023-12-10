0f17607fb0d271b1fef8daa887b8dff88056899a
Leo-Youmbi <leoyoumbi83@gmail.com>
2023-12-10 08:26:23 +0100

    refactor: added credits footer

--- connect-4-clone/src/components/Footer.jsx
+++ connect-4-clone/src/components/Footer.jsx
@@ -7,10 +7,14 @@ const Footer = ({newGameClickEvent, suggestClickEvent, gameState}) => {
     let clickEvent;
     let buttonText;
     [clickEvent, buttonText] = gameState === GAME_STATE_PLAYING ? [suggestClickEvent, 'Suggest'] : [newGameClickEvent, 'New Game']
+    let footTest = 'Made with love by Youmbi Leo <3  :)'
     return (
+        <>
         <div className='panel footer'>
         <button type='button' onClick={clickEvent}>{buttonText}</button>
         </div>
+        <footer>{footTest}</footer>
+        </>
     );
 };
 
