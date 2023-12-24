04a5d9c7b8c0bad3a789cd55e48f9062a1d502dc
Leo-Youmbi <leoyoumbi83@gmail.com>
2023-12-24 14:33:39 +0100

    product details

--- e-store/src/components/productDetails.jsx
+++ e-store/src/components/productDetails.jsx
@@ -1,21 +1,71 @@
-import React, { useState } from 'react';
+// eslint-disable-next-line no-unused-vars
+import React, {useEffect, useState} from 'react';
 import { useParams } from 'react-router-dom';
 import { getProductById } from '../fetcher';
 
 const ProductDetails = () => {
-    const [products, setProducts] = useState({errorMessage: '', data: []})
+    const [product, setProduct] = useState({errorMessage: '', data: {}})
     const {productId} = useParams();
 
-    React.useEffect(() => {
+    useEffect(() => {
         const fetchData = async () => {
             const responseObject = await getProductById(productId)
-            setProducts(responseObject);
+            setProduct(responseObject);
         }
         fetchData();
     }, [productId])
-
+    console.log(product)
     return (
-        <div>ProductDetail {productId} title:{products.data.title}</div>
+        <article>
+            <div className='category-product-title'>
+                {product.data.title}
+            </div>
+            <figure>
+                <div className='category-product-image-container'>
+                    <img src={`../assets/${product.data.image}`} alt={product.data.title} />
+                </div>
+            </figure>
+            <aside>
+                <div className='category-product-info-dimensions'>
+                    <h3>Dimensions</h3>
+                    <label> {product.data.specs?.dimensions} </label>
+                </div>
+                { product.data.specs &&
+                <div className='category-product-info-capacity'>
+                    <h3>Capacity</h3>
+                    <label> {product.data.specs.capacity} </label>
+                </div>
+                }
+                { product.data.features &&
+                <div className='category-product-info-features'>
+                    <h3>Features</h3>
+                    <ul>
+                        {
+                            product.data.features.map((f, index) => {
+                                return <li key={`feature-${index}`} >{f}</li>
+                            })
+                        }
+                    </ul>
+                </div>
+                }
+            </aside>
+            <aside className='category-product-finance'>
+                <div className='category-product-finance-price'>
+                    &pound;{product.data.price}
+                </div>
+
+                <div className='category-product-info-stock'>
+                    <label>Stock Level: {product.data.stock}</label>
+                    <label>FREE Delivery</label>
+                </div>
+
+                <div className='category-product-action'>
+                    <button>Add to Basket</button>
+                </div>
+            </aside>
+
+            <div>{ product.data?.description }</div>
+        </article>
     );
 };
 
