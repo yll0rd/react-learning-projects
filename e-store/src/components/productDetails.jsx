19b05293fc4f5e3589f59cdb0d1ca30e48a80a7f
Leo-Youmbi <leoyoumbi83@gmail.com>
2023-12-24 10:18:34 +0100

    getProductBy Id

--- e-store/src/components/productDetails.jsx
+++ e-store/src/components/productDetails.jsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { getProductById } from '../fetcher';
 
 const ProductDetails = () => {
+    const [products, setProducts] = useState({errorMessage: '', data: []})
+    const {productId} = useParams();
+
+    React.useEffect(() => {
+        const fetchData = async () => {
+            const responseObject = await getProductById(productId)
+            setProducts(responseObject);
+        }
+        fetchData();
+    }, [productId])
+
     return (
-        <div>ProductDetail</div>
+        <div>ProductDetail {productId} title:{products.data.title}</div>
     );
 };
 
