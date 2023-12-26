419e1001e166cea017a5623f51f7b512bd9b6e1e
Leo-Youmbi <leoyoumbi83@gmail.com>
2023-12-26 17:20:59 +0100

    cartContext intro

--- e-store/src/main.jsx
+++ e-store/src/main.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import CartContextProvider from './contexts/cartContext.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <App />
+      <CartContextProvider>
+        <App />
+      </CartContextProvider>
   </React.StrictMode>,
 )
