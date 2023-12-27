2a4f68f5f4b326e19c9b3c490ff8be3c87fcf8ac
Leo-Youmbi <leoyoumbi83@gmail.com>
2023-12-27 03:57:22 +0100

    removing React Strictmode

--- e-store/src/main.jsx
+++ e-store/src/main.jsx
@@ -6,9 +6,9 @@ import CartContextProvider from './contexts/cartContext.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // <React.StrictMode>
       <CartContextProvider>
         <App />
       </CartContextProvider>
-  </React.StrictMode>,
+  // </React.StrictMode>,
 )
