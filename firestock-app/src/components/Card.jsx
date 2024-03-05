4a333ebe48a42c4dbaef5afac334015673f61e27
Leo-Youmbi <leoyoumbi83@gmail.com>
2024-03-05 04:53:21 +0100

    feat: updated card ui

--- firestock-app/src/components/Card.jsx
+++ firestock-app/src/components/Card.jsx
@@ -1,11 +1,28 @@
 // eslint-disable-next-line react/prop-types
+import {useMemo} from "react";
+
 function Card({photo}) {
     console.log(photo)
+    const { path, title, createdAt } = photo
+    const timestamp = useMemo(() => {
+        const date = `${new Date(createdAt.seconds * 1000)}`.split(" ")
+        return `${date[1]} ${date[2]} ${date[3]}`
+    }, [createdAt.seconds]);
     return (
         <div className='col mb-5'>
             <div className="card" style={{width: "18rem"}}>
+                <div style={{
+                    height: "220px",
+                    backgroundImage: `url(${path})`,
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat"
+                }}></div>
                 {/* eslint-disable-next-line react/prop-types */}
-                <img src={photo.path} className="card-img-top" alt={photo.title} />
+                <h5 className="text-center mt-1">{title}</h5>
+                <div className="d-flex justify-content-between p-2">
+                    <p>{timestamp}</p>
+                    <i>@username</i>
+                </div>
             </div>
         </div>
     )
